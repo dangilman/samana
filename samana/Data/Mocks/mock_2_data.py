f775016def7153098e589381986b5486eaa41696
Daniel Gilman <gilmanda@ucla.edu>
2024-05-08 15:20:23 -0500

    update mocks 2 and 9

--- samana/Data/Mocks/mock_2_data.py
+++ samana/Data/Mocks/mock_2_data.py
@@ -2,6 +2,7 @@ import numpy as np
 from samana.Data.Mocks.base import MockBase
 from samana.Data.ImageData.MockImageData.mock_2_simple import image_data as simple_image_data
 from samana.Data.ImageData.MockImageData.mock_2_cosmos import image_data as cosmos_image_data
+from samana.Data.ImageData.MockImageData.mock_2_cosmos import image_data_new as cosmos_image_data_new
 from samana.Data.ImageData.MockImageData.mock_2_2038 import image_data as simulated_2038_image_data
 from samana.Data.ImageData.MockImageData.mock_2_cosmos_psf3 import image_data as cosmos_image_data_psf3
 from samana.Data.ImageData.MockImageData.mock_2_cosmos_wdm import image_data as cosmos_image_data_wdm
@@ -74,5 +75,33 @@ class Mock2DataWDM(MockBase):
         else:
             raise Exception('only cosmos source implemented for this class')
         super(Mock2DataWDM, self).__init__(z_lens, z_source, x_image, y_image,
+                                    magnifications, astrometric_uncertainties, flux_ratio_uncertainties,
+                                        image_data, super_sample_factor)
+
+class Mock2DataNew(MockBase):
+    """
+    Created with pyHalo commit d9772f6
+    Includes the Galacticus truncation model applied to CDM subhalos
+    """
+    def __init__(self, super_sample_factor=1.0, cosmos_source=True):
+
+        z_lens = 0.4
+        z_source = 1.7
+        x_image = np.array([ 0.91094244, -0.87994455, -0.69009147,  0.16632836])
+        y_image = np.array([ 0.75072815, -0.24933848,  0.53528928, -0.78094243])
+        magnifications_true = np.array([3.02539226, 6.51658887, 4.86756372, 2.3252286 ])
+        magnification_measurement_errors = 0.0
+        magnifications = np.array(magnifications_true) + np.array(magnification_measurement_errors)
+        astrometric_uncertainties = [0.003] * 4
+        flux_ratio_uncertainties = None
+        self.a3a_true = 0.0002277
+        self.a4a_true = -0.004348
+        self.delta_phi_m3_true = -0.067025
+        self.delta_phi_m4_true = 0.0
+        if cosmos_source:
+            image_data = cosmos_image_data_new
+        else:
+            raise Exception('only cosmos source implemented for this class')
+        super(Mock2DataNew, self).__init__(z_lens, z_source, x_image, y_image,
                                     magnifications, astrometric_uncertainties, flux_ratio_uncertainties,
                                         image_data, super_sample_factor)
--- samana/Data/Mocks/mock_9_data.py
+++ samana/Data/Mocks/mock_9_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 from samana.Data.Mocks.base import MockBase
 from samana.Data.ImageData.MockImageData.mock_9_cosmos import image_data as cosmos_image_data
+from samana.Data.ImageData.MockImageData.mock_9_cosmos import image_data_new as cosmos_image_data_new
 from samana.Data.ImageData.MockImageData.mock_9_cosmos_wdm import image_data as cosmos_image_data_wdm
 
 
@@ -18,7 +19,7 @@ class Mock9Data(MockBase):
         astrometric_uncertainties = [0.003] * 4
         flux_ratio_uncertainties = None
 
-        self.a3a_true = -0.005
+        self.a3a_true = 0.005
         self.a4a_true = -0.01
         self.delta_phi_m3_true = -0.5127
         self.delta_phi_m4_true = 0.0
@@ -57,3 +58,32 @@ class Mock9DataWDM(MockBase):
         super(Mock9DataWDM, self).__init__(z_lens, z_source, x_image, y_image,
                                 magnifications, astrometric_uncertainties, flux_ratio_uncertainties,
                                 image_data, super_sample_factor)
+
+class Mock9DataNew(MockBase):
+    """
+    Created with pyHalo commit d9772f6
+    Includes the Galacticus truncation model applied to CDM subhalos
+    """
+    def __init__(self, super_sample_factor=1.0, cosmos_source=True):
+
+        z_lens = 0.6
+        z_source = 1.79
+        x_image = np.array([ 0.79472023, -0.63149182,  0.12679314, -0.06701674])
+        y_image = np.array([ 0.74266127,  0.8302074 ,  1.06685962, -0.75195143])
+        magnifications_true = np.array([ 9.35072724, 11.06000803, 14.14145089,  1.95894006])
+        magnification_measurement_errors = 0.0
+        magnifications = np.array(magnifications_true) + np.array(magnification_measurement_errors)
+        astrometric_uncertainties = [0.003] * 4
+        flux_ratio_uncertainties = None
+        self.a3a_true = 0.005
+        self.a4a_true = -0.01
+        self.delta_phi_m3_true = -0.5127
+        self.delta_phi_m4_true = 0.0
+        if cosmos_source:
+            image_data = cosmos_image_data_new
+        else:
+            raise Exception('only cosmos source implemented for this class')
+        super(Mock9DataNew, self).__init__(z_lens, z_source, x_image, y_image,
+                                    magnifications, astrometric_uncertainties, flux_ratio_uncertainties,
+                                        image_data, super_sample_factor)
+
