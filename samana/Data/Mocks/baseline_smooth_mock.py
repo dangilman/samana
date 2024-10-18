36c5f15ecdbff56bf2e08a9af6a5f679893621c2
Daniel Gilman <gilmanda@ucla.edu>
2024-10-18 15:24:15 -0500

    add new smooth mock

--- samana/Data/Mocks/baseline_smooth_mock.py
+++ samana/Data/Mocks/baseline_smooth_mock.py
@@ -24,6 +24,28 @@ class BaselineSmoothMock(MockBase):
                                                            flux_ratio_uncertainties, image_data,
                                                            super_sample_factor)
 
+class BaselineSmoothMockv2(MockBase):
+    """This mock has a relatively large ellipticity q = 0.4"""
+    def __init__(self, super_sample_factor=1.0):
+        z_lens = 0.5
+        z_source = 2.0
+        x_image = np.array([ 1.20615909, -0.79343069, -0.16863934, -0.08341527])
+        y_image = np.array([ 0.24720679,  0.37340355,  0.82242164, -0.56724028])
+        magnifications_true = np.array([1.67270303, 2.68093683, 1.12174305, 0.45608223])
+        from samana.Data.ImageData.baseline_smooth_mock_v2 import image_data
+        magnification_measurement_errors = 0.0
+        magnifications = np.array(magnifications_true) + np.array(magnification_measurement_errors)
+        astrometric_uncertainties = [0.00001] * 4
+        flux_ratio_uncertainties = None
+        self.a3a_true = -0.0
+        self.a4a_true = 0.00
+        self.delta_phi_m3_true = 0.0
+        self.delta_phi_m4_true = 0.0
+        super(BaselineSmoothMockv2, self).__init__(z_lens, z_source, x_image, y_image,
+                                                           magnifications, astrometric_uncertainties,
+                                                           flux_ratio_uncertainties, image_data,
+                                                           super_sample_factor)
+
 class BaselineSmoothMockMultipole1(MockBase):
 
     def __init__(self, super_sample_factor=1.0):
@@ -124,7 +146,6 @@ class BaselineSmoothMockMultipole4(MockBase):
                                     magnifications, astrometric_uncertainties, flux_ratio_uncertainties, image_data,
                                         super_sample_factor)
 
-
 class BaselineSmoothMockModel(MockModelBase):
 
     def setup_source_light_model(self):
@@ -176,3 +197,37 @@ class BaselineSmoothMockModel(MockModelBase):
         lens_model_params = [kwargs_lens_init, kwargs_lens_sigma, kwargs_lens_fixed, kwargs_lower_lens,
                              kwargs_upper_lens]
         return lens_model_list_macro, redshift_list_macro, index_lens_split, lens_model_params
+
+class BaselineSmoothMockModel_EllipticalMultipole(BaselineSmoothMockModel):
+
+    def setup_lens_model(self, kwargs_lens_macro_init=None, macromodel_samples_fixed=None):
+        lens_model_list_macro = ['EPL_MULTIPOLE_M3M4_ELL', 'SHEAR']
+        kwargs_lens_macro = [{'theta_E': 1.0, 'center_x': 0.0, 'center_y': 0.0, 'e1': -0.1, 'e2': -0.1,
+                              'gamma': 2.0, 'a4_a': 0.0,
+                              'a3_a': 0.0, 'delta_phi_m3': 0.0,
+                              'delta_phi_m4': 0.0}, {'gamma1': -0.04, 'gamma2': 0.04}]
+        redshift_list_macro = [self._data.z_lens, self._data.z_lens]
+        index_lens_split = [0, 1]
+        if kwargs_lens_macro_init is not None:
+            for i in range(0, len(kwargs_lens_macro_init)):
+                for param_name in kwargs_lens_macro_init[i].keys():
+                    kwargs_lens_macro[i][param_name] = kwargs_lens_macro_init[i][param_name]
+        kwargs_lens_init = kwargs_lens_macro
+        kwargs_lens_sigma = [{'theta_E': 0.05, 'center_x': 0.1, 'center_y': 0.1, 'e1': 0.1, 'e2': 0.1, 'gamma': 0.1,
+                              'a4_a': 0.01, 'a3_a': 0.005, 'delta_phi_m3': np.pi / 12, 'delta_phi_m4': np.pi / 16},
+                             {'gamma1': 0.05, 'gamma2': 0.05}]
+        kwargs_lens_fixed = [{'delta_phi_m4': 0.0}, {'ra_0': 0.0, 'dec_0': 0.0}]
+        kwargs_lower_lens = [
+            {'theta_E': 0.05, 'center_x': -10.0, 'center_y': -10.0, 'e1': -0.5, 'e2': -0.5, 'gamma': 1.5, 'a4_a': -0.1,
+             'a3_a': -0.1, 'delta_phi_m3': -np.pi / 6, 'delta_phi_m4': -10.0},
+            {'gamma1': -0.5, 'gamma2': -0.5}]
+        kwargs_upper_lens = [
+            {'theta_E': 5.0, 'center_x': 10.0, 'center_y': 10.0, 'e1': 0.5, 'e2': 0.5, 'gamma': 3.5, 'a4_a': 0.1,
+             'a3_a': 0.1, 'delta_phi_m3': np.pi / 6, 'delta_phi_m4': 10.0},
+            {'gamma1': 0.5, 'gamma2': 0.5}]
+        kwargs_lens_fixed, kwargs_lens_init = self.update_kwargs_fixed_macro(lens_model_list_macro, kwargs_lens_fixed,
+                                                                             kwargs_lens_init, macromodel_samples_fixed)
+        lens_model_params = [kwargs_lens_init, kwargs_lens_sigma, kwargs_lens_fixed, kwargs_lower_lens,
+                             kwargs_upper_lens]
+        return lens_model_list_macro, redshift_list_macro, index_lens_split, lens_model_params
+
