5021b6a3f9bea73a90960b176352ff114f4f4a4c
Daniel Gilman <gilmanda@ucla.edu>
2025-08-29 16:48:08 -0500

    update j2344 with new position

--- samana/Data/j2344.py
+++ samana/Data/j2344.py
@@ -15,7 +15,7 @@ class _J2344(ImagingDataBase):
         if image_data_type == 'HST814W':
             raise Exception('no hst imaging data for this system')
 
-        elif image_data_type == 'MIRI540W':
+        elif image_data_type == 'MIRI560W':
             from samana.Data.ImageData.j2344_MIRI540W import psf_model, image_data, noise_map
             self._psf_estimate_init = psf_model
             self._psf_error_map_init = None
@@ -31,7 +31,7 @@ class _J2344(ImagingDataBase):
             self._exposure_time = None
             self._noise_map = noise_map
         else:
-            raise Exception('image data type must be either HST814W or MIRI540W')
+            raise Exception('image data type must be either HST814W or MIRI560W')
 
         self._supersample_factor = supersample_factor
 
@@ -121,39 +121,6 @@ class J2344_MIRI(_J2344):
 
         x_image = np.array([-0.41950206, 0.17051697, 0.46399316, -0.17500807])
         y_image = np.array([0.13005771, 0.47974816, -0.19611058, -0.53369529])
-        horizontal_shift = 0.01
-        vertical_shift = 0.0
-        x_image += horizontal_shift
-        y_image += vertical_shift
-        image_position_uncertainties = [0.005] * 4  # 5 arcsec
-        flux_uncertainties = None
-        uncertainty_in_fluxes = False
-        magnifications = np.array([1.0] * 4)
-        image_data_type = 'MIRI540W'
-        super(J2344_MIRI, self).__init__(x_image,
-                                         y_image,
-                                         magnifications,
-                                         image_position_uncertainties,
-                                         flux_uncertainties,
-                                         uncertainty_in_fluxes,
-                                         supersample_factor,
-                                         image_data_type)
-
-class J2344_MIRI_NEWPOS(_J2344):
-
-    def __init__(self, supersample_factor=1):
-        """
-
-        :param image_position_uncertainties: list of astrometric uncertainties for each image
-        i.e. [0.003, 0.003, 0.003, 0.003]
-        :param flux_uncertainties: list of flux ratio uncertainties in percentage, or None if these are handled
-        post-processing
-        :param magnifications: image magnifications; can also be a vector of 1s if tolerance is set to infintiy
-        :param uncertainty_in_fluxes: bool; the uncertainties quoted are for fluxes or flux ratios
-        """
-
-        x_image = np.array([-0.41950206, 0.17051697, 0.46399316, -0.17500807])
-        y_image = np.array([0.13005771, 0.47974816, -0.19611058, -0.53369529])
         delta_x = np.array([0.0, -0.005, -0.007, -0.003])
         delta_y = np.array([0.0, -0.001, 0.003, 0.003])
         x_image += delta_x
@@ -166,8 +133,8 @@ class J2344_MIRI_NEWPOS(_J2344):
         flux_uncertainties = None
         uncertainty_in_fluxes = False
         magnifications = np.array([1.0] * 4)
-        image_data_type = 'MIRI540W'
-        super(J2344_MIRI_NEWPOS, self).__init__(x_image,
+        image_data_type = 'MIRI560W'
+        super(J2344_MIRI, self).__init__(x_image,
                                          y_image,
                                          magnifications,
                                          image_position_uncertainties,
